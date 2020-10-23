09960d7623035dbf764ec1392c2536d79714299e
katkoffff <71480869+katkoffff@users.noreply.github.com>
2020-10-23 10:18:40 +0500

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,12 @@
+class Cat:
+    def __init__(self,name,male,age):
+        self.name=name
+        self.male=male
+        self.age=age        
+    def getName(self):
+        return self.name
+    def getMale(self):
+        return self.male
+    def getAge(self):
+        return self.age
+        
---
+++ test_cat.py
@@ -0,0 +1,7 @@
+from cat import Cat
+
+cat_1=Cat('Барон','мальчик','2 года')
+cat_2=Cat('Сэм','мальчик','2 года')
+
+print(f"имя: {cat_1.getName()}, пол: {cat_1.getMale()} возраст: {cat_1.getAge()}")
+print(f"имя: {cat_2.getName()}, пол: {cat_2.getMale()} возраст: {cat_2.getAge()}")
